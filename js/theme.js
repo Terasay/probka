6491963b2f07bb1a1c6a435779d598977d09610d
Terasay <limonorigin@gmail.com>
2025-09-22 12:09:57 +0500

    Добавлены новые темы оформления: киберпанк и глубокая тьма. Обновлены кнопки переключения тем на странице аккаунта и реализована поддержка новых тем в JavaScript.

--- js/theme.js
+++ js/theme.js
@@ -5,6 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!themeLink) return;
     themeLink.href = "css/theme-" + theme + ".css";
     localStorage.setItem("theme", theme);
+      // Удаляем все классы тем с body
+      document.body.classList.remove(
+        "theme-dark",
+        "theme-pink",
+        "theme-light",
+        "theme-gray",
+        "theme-red",
+        "theme-cyberpank",
+        "theme-deep-dark"
+      );
+      document.body.classList.add("theme-" + theme);
   }
 
   const btnDark = document.getElementById("theme-dark");
@@ -12,10 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnGray = document.getElementById("theme-gray");
   const btnRed = document.getElementById("theme-red");
   const btnPink = document.getElementById("theme-pink");
+  const btnCyberpank = document.getElementById("theme-cyberpank");
+  const btnDeepDark = document.getElementById("theme-deep-dark");
 
   if (btnDark) btnDark.addEventListener("click", () => setTheme("dark"));
   if (btnLight) btnLight.addEventListener("click", () => setTheme("light"));
   if (btnGray) btnGray.addEventListener("click", () => setTheme("gray"));
   if (btnRed) btnRed.addEventListener("click", () => setTheme("red"));
   if (btnPink) btnPink.addEventListener("click", () => setTheme("pink"));
+  if (btnCyberpank) btnCyberpank.addEventListener("click", () => setTheme("cyberpank"));
+  if (btnDeepDark) btnDeepDark.addEventListener("click", () => setTheme("deep-dark"));
 });
