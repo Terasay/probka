1ff824c2a5d0adda61969849d303fa726bb24960
Terasay <limonorigin@gmail.com>
2025-10-25 12:25:22 +0500

    Улучшено отображение ответов на сообщения; добавлено отображение вложений с изображениями и файлами в ответах.

--- js/messenger.js
+++ js/messenger.js
@@ -550,7 +550,23 @@ function renderMessages() {
 				replyDiv.style.marginBottom = '4px';
 				replyDiv.style.fontSize = '0.95em';
 				replyDiv.style.color = '#bfc9d8';
-				replyDiv.innerHTML = `<span style=\"font-weight:500;\">${escapeHtml(repliedMsg.sender_name)}:</span> ${escapeHtml(repliedMsg.content).slice(0, 48)}${repliedMsg.content.length > 48 ? '…' : ''}`;
+
+				let replyHtml = `<span style="font-weight:500;">${escapeHtml(repliedMsg.sender_name)}:</span> `;
+				if (repliedMsg.content) {
+					replyHtml += escapeHtml(repliedMsg.content).slice(0, 48);
+					if (repliedMsg.content.length > 48) replyHtml += '…';
+				}
+				if (repliedMsg.files && Array.isArray(repliedMsg.files) && repliedMsg.files.length) {
+					repliedMsg.files.forEach(file => {
+						if (typeof file === 'string' && (file.endsWith('.jpg') || file.endsWith('.jpeg') || file.endsWith('.png') || file.endsWith('.gif') || file.endsWith('.webp'))) {
+							replyHtml += `<img src="${file}" style="max-width:32px;max-height:32px;border-radius:5px;margin-left:6px;vertical-align:middle;box-shadow:0 1px 4px #0002;">`;
+						} else if (typeof file === 'string') {
+							const fname = file.split('/').pop();
+							replyHtml += `<span style="display:inline-block;background:#202a36;color:#229ed9;border-radius:5px;padding:2px 7px;font-size:0.92em;margin-left:6px;vertical-align:middle;"><svg style="width:1em;height:1em;vertical-align:middle;margin-right:2px;opacity:0.7;" viewBox="0 0 24 24"><path fill='#229ed9' d='M6 2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6H6zm7 1.5V9h5.5L13 3.5zM6 4h6v5a2 2 0 0 0 2 2h5v11a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1z'/></svg>${escapeHtml(fname)}</span>`;
+						}
+					});
+				}
+				replyDiv.innerHTML = replyHtml;
 				div.appendChild(replyDiv);
 			}
 		}
